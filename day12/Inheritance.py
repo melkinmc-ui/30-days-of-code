7452b837be4292bca314a351a0789aad3a83fd41
melkinmc-ui <melkinmc@gmail.com>
2026-08-21 00:38:08 +0000

    retos completados de hackerank

---
+++ day12/Inheritance.py
@@ -0,0 +1,34 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        if not self.scores:
+            average = 0
+        else:
+            average = sum(self.scores) / len(self.scores)
+
+        if average >= 90:
+            return 'O'
+        elif average >= 80:
+            return 'E'
+        elif average >= 70:
+            return 'A'
+        elif average >= 55:
+            return 'P'
+        elif average >= 40:
+            return 'D'
+        else:
+            return 'T'
+
+line = input().split()
